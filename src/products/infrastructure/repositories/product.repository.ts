62556a09fbb71d0587b1cbd2f743863aa000c01d
Harold Andres Jara <andresjara640@gmail.com>
2026-02-28 15:28:08 -0500

    Se actualiza nombre de ruta del controlador y nombre de los servicios, se adiciona servicio de CORS al main.ts

--- src/products/infrastructure/repositories/product.repository.ts
+++ src/products/infrastructure/repositories/product.repository.ts
@@ -3,20 +3,16 @@ import { PrismaService } from 'src/prisma.service';
 import { ProductDTO } from '../dto/product.dto';
 
 @Injectable()
-export class ProductRepository  {
- 
-
+export class ProductRepository {
   constructor(private readonly prisma: PrismaService) {}
-    
-  getAllMockProducts(){
+
+  getAllProducts() {
     return this.prisma.products.findMany();
   }
 
-  createProduct(product: ProductDTO){
+  createProduct(product: ProductDTO) {
     return this.prisma.products.create({
       data: product,
     });
   }
-
-  
 }
